c7f466be9afda128dff1552a6b9f41175e1d6c57
Himanshu kumar <112979593+himanshusain0@users.noreply.github.com>
2023-10-31 10:48:54 +0530

    Add files via upload

---
+++ news.js
@@ -0,0 +1,77 @@
+const API_KEY="e2b1000ec6d64306bee1cca91217d371";
+const url="https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load",()=>fetchNews("India"));
+
+function reload(){
+    window.location.reload();
+}
+
+async function fetchNews(query){
+    const res=await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data=await res.json();
+    bindData(data.articles);
+    console.log(data.articles);
+}
+
+function bindData(articles){
+    const cardsContainer=document.getElementById('cards-container');
+    const newsCardsTemplate=document.getElementById('template-news-card');
+
+    // const cardClone=newsCardsTemplate.content.cloneNode(true);
+    // console.log(cardClone);
+    
+    
+    cardsContainer.innerHTML='';
+
+     articles.forEach((article)=>{
+         if(!article.urlToImage) return;
+         const cardClone=newsCardsTemplate.content.cloneNode(true);
+         fillDataInCard(cardClone,article);
+         cardsContainer.appendChild(cardClone);
+         console.log(cardClone);
+        //  fillDataInCard(cardsContainer,article);
+     })
+// }
+ function fillDataInCard(cardClone,article){
+     const newsImg=cardClone.querySelector('#news-img');
+     const newsTitle=cardClone.querySelector('#news-title');
+     const newsSource=cardClone.querySelector('#news-source');
+     const newsDesc=cardClone.querySelector('#news-desc');
+
+     newsImg.src=article.urlToImage;
+     newsTitle.innerHTML=article.title;
+     newsDesc.innerHTML=article.description;
+
+     const date=new Date(article.publishedAt).toLocaleString("en-US",{
+         timeZone:"Asia/Jakarta"
+     });
+     newsSource.innerHTML=`${article.source.name} .${date}`;
+
+
+     cardClone.firstElementChild.addEventListener("click",()=>{
+        window.open(article.url,"_blank");
+     })
+
+ }
+ }
+let curSelectedNav=null;
+function onNavItemClick(id){
+    fetchNews(id);
+    const navItem=document.getElementById(id);
+    curSelectedNav?.classList.remove('active');
+    curSelectedNav=navItem;
+    curSelectedNav.classList.add('active');
+
+}
+
+const searchButton=document.getElementById('search-button');
+const searchText=document.getElementById('search-text');
+
+searchButton.addEventListener('click',()=>{
+    const query=searchText.value;
+    if(!query) return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove('active');
+    curSelectedNav=null;
+})
